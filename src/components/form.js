d66e5faab901bb8ee1915d1cce565ceef6d1f3a0
alokanand007 <personal4paper@gmail.com>
2024-10-03 16:38:12 +0530

    validation

--- src/components/form.js
+++ src/components/form.js
@@ -1,51 +1,102 @@
 import { Link } from "react-router-dom";
 import eye from "../assets/eye.png";
-import closeeye from "../assets/closeeye.png"
-import { useMemo, useState } from "react";
-
+import closeeye from "../assets/closeeye.png";
+import { useMemo, useState, useEffect } from "react";
+import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
 
 function Form() {
-  const [visible ,notVisible]=useState("password");
-   
-  const eyeF= ()=> {
+  const intial = { email: "", password: "" };
+  const [formvalues, setFormValues] = useState(intial);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [visible, notVisible] = useState("password");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formvalues, [name]: value });
+    console.log(formvalues);
+  };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formvalues));
+    setIsSubmit(true);
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    if (!values.email) {
+      errors.email = "Email is required";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+
+    return errors;
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formvalues);
+    }
+  }, [formErrors]);
+
+  const eyeF = () => {
     if (visible == "password") {
       notVisible("text");
-    }
-    else{
+    } else {
       notVisible("password");
     }
-  }
+  };
 
-const passeye=useMemo(()=>{
-  if (visible == "password") {
-  return closeeye;
-  }
-  else{
-  return eye;
-  }
-},[visible])
-  
+  const passeye = useMemo(() => {
+    if (visible == "password") {
+      return closeeye;
+    } else {
+      return eye;
+    }
+  }, [visible]);
+
   return (
     <form>
       <div className="mt-14 border-2 clo pl-4 pt-1 pb-2 rounded-sm">
-        <label>
-          Enter your Email:
-          <br />
-          <input className="outline-" type="text" />
-          <br />
-        </label>
+        <label>Email:</label>
+       
+        <br />
+        <input
+          className="outline-"
+          type="text"
+          name="email"
+          values={formvalues.email}
+          onChange={handleChange}
+        />
+        <br />
+      <p className="text-red-600">{formErrors.email}</p>
       </div>
-
       <div className="border-t-0 border-2  pl-4 pt-1 pb-2 clo rounded-sm">
-        <label>
-          {" "}
-          Enter your password:
+        <label>password: </label>
+        
+         
           <br />
           <div className="password">
-          <input className="outline- text-blue-600 " type={visible} id="password"/>{" "}
-          <img src={passeye} onClick={eyeF} className="pass-icon" id="pass-icon" alt="sd"></img>
+            <input
+              className="outline- text-blue-600 "
+              type={visible}
+              id="password"
+              values={formvalues.password}
+              name="password"
+              onChange={handleChange}
+            />
+            <img
+              src={passeye}
+              onClick={eyeF}
+              className="pass-icon"
+              id="pass-icon"
+              alt="sd"
+            ></img>
           </div>
-        </label>
+          <p className="text-red-600">{formErrors.password}</p>
       </div>
 
       <div className="flex mt-5 justify-between">
@@ -56,14 +107,14 @@ const passeye=useMemo(()=>{
         <p className=" clo text-sm font-roboto">Forgot password?</p>
       </div>
       <div className="grr mt-12">
-        
-          <button
-            className=" text-sm  btncolor font-roboto w-20 h-9 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..."
-            type="button"
-          >
-            Login
-          </button>
-      
+        <button
+          className=" text-sm  btncolor font-roboto w-20 h-9 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..."
+          type="button"
+          onClick={handlesubmit}
+        >
+          Login
+        </button>
+
         <Link to="/sign-up">
           <button
             className=" border-2  text-sm font-roboto w-20 h-9  active:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300 ..."
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,68 +1,131 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Signup() {
+  const intial = { firstname: "", lastname: "", email: "", password: "" };
+  const [formvalues, setFormValues] = useState(intial);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formvalues, [name]: value });
+    console.log(formvalues);
+  };
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formvalues));
+    setIsSubmit(true);
+  };
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    if (!values.firstname) {
+      errors.firstname = "Firstname is required";
+    }
+    if (!values.lastnamename) {
+      errors.lastname = "Lastname is required";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+
+    return errors;
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formvalues);
+    }
+  }, [formErrors]);
+
   return (
     <div>
       <form>
         <div className="gap-x-6 flex  ">
-          <div  className="mt-8 border-2 clo pl-4 pt-1 pb-2 w-1/2 rounded-sm">
-            <label>
-              First Name:
-              <br />
-              <input className="outline-" type="text" />
-              <br />
-            </label>
+          <div className="mt-8 border-2 clo pl-4 pt-1 pb-2 w-1/2 rounded-sm">
+            <label> First Name: </label>
+
+            <br />
+            <input
+              className="outline-"
+              type="text"
+              name="firstname"
+              values={formvalues.firstname}
+              onChange={handleChange}
+            />
+            <br />
+            <p className="text-red-600">{formErrors.firstname}</p>
           </div>
 
-           <div  className="mt-8 border-2 clo pl-4 pt-1 pb-2 w-1/2 rounded-sm">
-          <label>
-            Last Name:
-            <br />
-            <input className="outline-" type="text" />
-            <br />
-          </label>
-            </div>
+          <div className="mt-8 border-2 clo pl-4 pt-1 pb-2 w-1/2 rounded-sm">
+            <label> Last Name: </label>
 
+            <br />
+            <input
+              className="outline-"
+              type="text"
+              name="lastname"
+              values={formvalues.lastname}
+              onChange={handleChange}
+            />
+            <br />
+            <p className="text-red-600">{formErrors.lastname}</p>
+          </div>
         </div>
 
         <div>
+          <div className="mt-4 border-2 clo pl-4 pt-1 pb-2 rounded-sm">
+            <label> Enter your Email: </label>
 
-        <div className="mt-4 border-2 clo pl-4 pt-1 pb-2 rounded-sm">
-          <label>
-            Enter your Email:
             <br />
-            <input className="outline-" type="text" />
+            <input
+              className="outline-"
+              type="text"
+              name="email"
+              values={formvalues.email}
+              onChange={handleChange}
+            />
             <br />
-          </label>
-        </div>
+            <p className="text-red-600">{formErrors.email}</p>
+          </div>
 
-        <div className="border-t-0 border-2  pl-4 pt-1 pb-2 clo rounded-sm">
-          <label>
-            {" "}
-            Enter your password:
+          <div className="border-t-0 border-2  pl-4 pt-1 pb-2 clo rounded-sm">
+            <label> Enter your password: </label>
+
             <br />
-            <input className="outline- text-blue-600 " type="text" />{" "}
-          </label>
-        </div>
+            <input
+              className="outline- text-blue-600 "
+              type="text"
+              name="password"
+              values={formvalues.password}
+              onChange={handleChange}
+            />
+            <p className="text-red-600">{formErrors.password}</p>
+          </div>
         </div>
 
         <div className="grr mt-12">
-            <button
-              className=" text-sm  btncolor font-roboto w-20 h-9 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..."
-              type="button"
-            >
-              <p>Sign up</p>
-            </button>
-            <Link to="/">
+          <button
+            className=" text-sm  btncolor font-roboto w-20 h-9 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..."
+            type="button"
+            onClick={handlesubmit}
+          >
+            Sign up
+          </button>
+          <Link to="/">
             <button
               className=" border-2  text-sm font-roboto w-20 h-9  active:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300 ..."
               type="button"
-              >
+            >
               Login
             </button>
-                </Link>
-          </div>
+          </Link>
+        </div>
       </form>
     </div>
   );
