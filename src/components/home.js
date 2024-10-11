061db59bda6b37895d3b5fb1e47f18abe4c9acfd
alokanand007 <personal4paper@gmail.com>
2024-10-11 19:24:45 +0530

    added nav

--- src/components/home.js
+++ src/components/home.js
@@ -1,60 +1,64 @@
 import React from "react";
-import github from "../assets/github.png";
-import user from "../assets/user.png";
-import instagram from "../assets/instagram.png"
-import LinkedIn from "../assets/linkedin.png"
-
+// import github from "../assets/github.png";
+// import instagram from "../assets/instagram.png";
+// import LinkedIn from "../assets/linkedin.png";
+import dp from "../assets/dp.png"
+import Myskills from "./myskills";
 
 
 function Home() {
   return (
     <div className="">
       {/* navbar */}
-           
 
-      <div className="flex justify-between mt-52 pl-52 pr-36">
-        <div>
-          <pre className="fontt">
-            Hi,
-            <br />
-          
-
-            I'am <span className="text-sky-600">Alok</span><br />
-            Web Developer
-          </pre>
-          <div className=""> 
-
-          <button
-            className="bg-sky-600 justify-center items-center w-40 h-12 rounded-md border-2 mt-16 flex text-sm font-roboto w-20 h-9  active:bg-violet-400 focus:outline-none "
-            type="button"
-            >
-            <p className="font-semibold text-lg text-white ">Contact</p>
-          </button>
+      <div className="grid grid-cols-2 gap-12 mt-32 mx-12 ">
+        <div className="flex justify-center">
+          <div className="col-span-1 pt-20">
+            <pre className="fontt ">
+              Hi,
+              <br />
+              I'am <span className="text-sky-600">Alok</span>
+              <br />
+              Web Developer
+            </pre>
+            <div className="">
+              <button
+                className="bg-sky-600 justify-center items-center w-48 h-16 rounded-xl border-2 mt-16 flex text-sm font-roboto w-20 h-9  active:bg-violet-400 focus:outline-none "
+                type="button"
+              >
+                <p className="font-semibold text-2xl text-white ">Contact</p>
+              </button>
             </div>
+          </div>
         </div>
-        <div>
-        <img src={user} alt="User" className=" h-4/5 	" />
-
+        <div className="col-span-1">
+          <div className="flex justify-center">
+            <img src={dp} alt="User" className=" w-4/5" />
+          </div>
         </div>
       </div>
 
       {/* link */}
-      <div className="">
+      {/* <div className="">
         <div className="pl-52 flex gap-x-5	">
-
-
-        <a href="http://www.linkedin.com">
-          <img className="h-6" src={LinkedIn} alt="ww"></img>
-        </a>
-        <a href="http://www.instagram.com">
-          <img className="h-6" src={instagram} alt="ww"></img>
-        </a>
-        <a href="http://www.github.com">
-          <img className="h-6" src={github} alt="ww"></img>
-        </a>
+          <a href="http://www.linkedin.com">
+            <img className="h-6" src={LinkedIn} alt="ww"></img>
+          </a>
+          <a href="http://www.instagram.com">
+            <img className="h-6" src={instagram} alt="ww"></img>
+          </a>
+          <a href="http://www.github.com">
+            <img className="h-6" src={github} alt="ww"></img>
+          </a>
         </div>
+      </div> */}
 
+      {/* tools */}
+      <div className="mt-32">
+
+      <Myskills/>
       </div>
+      {/* end */}
     </div>
   );
 }
--- src/components/navsection.js
+++ src/components/navsection.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Navsection() {
+  const [scroll, setscroll] = useState("");
+
+  const onscroll = () => {
+    if (window.scrollY < 15) {
+      setscroll("");
+    } else {
+      setscroll("shadow-lg");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onscroll);
+    return () => window.removeEventListener("scroll", onscroll);
+  }, []);
   return (
-    
-      <div className="flex justify-between fixed top-0 right-0 w-1/2  pt-12 px-14 www abt">
-          <Link  to="/" className="clo text-2xl font-semibold relative ">Home</Link>
-          <a href="www" className="clo text-2xl font-semibold relative ">About us</a>
-          <a href="www" className="clo text-2xl font-semibold relative ">Skills</a>
-          <a href="www" className="clo text-2xl font-semibold relative ">Works</a>
-          <Link  to="/contact_us" className="clo text-2xl font-semibold relative" >Contact</Link>
-        </div>
-    
-  )
+    <div
+      className={`${scroll} w-full bg-white fixed top-0 flex flex-row-reverse`}
+    >
+      <div className=" w-1/2 abt bg-white flex justify-between pb-4  pt-12 px-14">
+        <Link to="/" className="clo text-2xl font-semibold relative ">
+          Home
+        </Link>
+        <a href="www" className="clo text-2xl font-semibold relative ">
+          About us
+        </a>
+        <a href="www" className="clo text-2xl font-semibold relative ">
+          Skills
+        </a>
+        <a href="www" className="clo text-2xl font-semibold relative ">
+          Works
+        </a>
+        <Link to="/contact_us" className="clo text-2xl font-semibold relative">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navsection;
